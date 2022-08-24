63feabc43461e17b67ac636fdfed7cbc48ba3c4b
Muhammad Usman <ch.usman564@gmail.com>
2022-08-24 21:35:03 +0500

    Built Header for Desktop

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,5 +4,9 @@ module.exports = {
   plugins: [],
 };
 
-// PRIMARY #0b2c3d
+// purple-600 #5c3be7
 // SECONDARY #b39359
+// PURPLE BTN OUTLINE #6d4aff
+
+// CONTAINER
+// container px-8 md:px-12 lg:px-24 mx-auto text-[#494b7a]
