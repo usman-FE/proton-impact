969ed56b03ad95452291d6e0f5ab69ddedc646b4
Muhammad Usman <ch.usman564@gmail.com>
2022-08-26 21:21:51 +0500

    Built Header for larger and smaller devices

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,10 @@ module.exports = {
 // purple-800 #372580
 // purple-600 #5c3be7
 // purple-500 #6d4aff
+// purple-100 #e2dbff
 // Gray-900 #111827
+// Gray-400 #9ca3af
+// Gray-300 #d1d5db
 // SECONDARY #b39359
 // PURPLE BTN OUTLINE #6d4aff
 
