baf059a7ea420ea95bca884d708c8951bf91f8aa
Muhammad Usman <ch.usman564@gmail.com>
2022-08-28 14:50:42 +0500

    Built Services section

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,3 +18,19 @@ module.exports = {
 // CONTAINER
 // container px-8 md:px-12 lg:px-24 mx-auto text-[#494b7a]
 // container grid gap-x-6 gap-y-12 md:gap-x-10 xl:gap:x-12
+
+
+// ANIMATED ANCHOR
+/*
+<a class="relative inline-block pr-5 text-[1.125rem] font-bold no-underline leading-5 focus:outline-none text-[#6d4aff] hover:text-[#5c3be7] focus:text-[#5c3be7]"
+  href="#">
+  <span
+    class="relative before:absolute before:w-full before:h-0.5 before:duration-300 before:bg-[#5c3be7] before:top-full before:left-0 before:transition-all before:scale-x-0 before:origin-left hover:before:scale-x-100">
+    Read Natalya’s story
+  </span>
+  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2"
+    stroke="currentColor" class="w-4 h-4 absolute top-1/2 right-0 -mt-2">
+    <path stroke-linecap="round" stroke-linejoin="round" d="M9 5l7 7-7 7"></path>
+  </svg>
+</a>
+*/
