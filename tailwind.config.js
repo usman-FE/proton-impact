201a58ca7db0cc9711765277e48150845ede79e2
Muhammad Usman <ch.usman564@gmail.com>
2022-08-28 10:54:15 +0500

    Built Hero section

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,7 @@ module.exports = {
 // purple-600 #5c3be7
 // purple-500 #6d4aff
 // purple-100 #e2dbff
+// purple-25 #f7f5ff
 // Gray-900 #111827
 // Gray-400 #9ca3af
 // Gray-300 #d1d5db
@@ -16,3 +17,4 @@ module.exports = {
 
 // CONTAINER
 // container px-8 md:px-12 lg:px-24 mx-auto text-[#494b7a]
+// container grid gap-x-6 gap-y-12 md:gap-x-10 xl:gap:x-12
